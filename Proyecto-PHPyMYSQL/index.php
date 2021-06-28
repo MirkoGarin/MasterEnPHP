d6c81a5e7b5c5c8c096db24c895eeae3131b7663
MirkoGarin <mirkogarin@gmail.com>
2021-06-27 23:33:00 -0300

    185. HTML del pie de página

---
+++ Proyecto-PHPyMYSQL/index.php
@@ -0,0 +1,105 @@
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Blog de Videos Juego</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<!-- CABECERA-->
+		<header id="cabezera" class="">
+			<div id ="logo">
+				<a href="index.php">Blog de Videojuegos</a>
+			</div>
+
+		<!--MENU-->
+			<nav id="menu">
+				<ul>
+					<li><a href="index.php" title="">Inicio</a></li>
+				</ul>
+				<ul>
+					<li><a href="index.php" title="">Categoria 1 </a></li>
+				</ul>
+				<ul>
+					<li><a href="index.php" title="">Categoria 2</a></li>
+				</ul>
+				<ul>
+					<li><a href="index.php" title="">Categoria 3 </a></li>
+				</ul>
+				<ul>
+					<li><a href="index.php" title="">Sobre mi</a></li>
+				</ul>
+				<ul>
+					<li><a href="index.php" title="">Contacto</a></li>
+				</ul>
+
+			</nav>
+		</header>
+
+		<div id="contenedor">
+			
+		
+		<!-- BARRA LATERAL -->
+		<aside id="sidebar">
+			<div id="login" class="bloque">
+				<h3>Identificate</h3> 
+				<form action="login.php" method="POST" accept-charset="utf-8">
+					<label for="email">Email</label>
+					<input type="email" name="email"/>
+					<label for="password">Contraseña</label>
+					<input type="password" name="password"/>
+					
+					<input type="submit" name="Entrar" value="Entrar" />
+					
+				</form>
+			</div>
+			<div id="login" class="bloque">
+				<h3>Registrate</h3> 
+				<form action="registro.php" method="POST" accept-charset="utf-8">
+					<label for="nombre">Nombre</label>
+					<input type="text" name="nombre"/>
+					<label for="apellidos">Apellidos</label>
+					<input type="text" name="apellidos"/>
+
+					<label for="email">Email</label>
+					<input type="email" name="email"/>
+					<label for="password">Contraseña</label>
+					<input type="password" name="password"/>
+					
+					<input type="submit" name="Entrar" value="Registrarse" />
+					
+				</form>
+			</div>
+			
+		</aside>
+
+		<!-- CAJA PRINCIPAL -->
+		<div id ="principal">
+			<h1>Ultimas entradas</h1>
+			<article class="entrada">
+				<h2>Titulo de mi entrada</h2>
+				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu mauris ac orci auctor vestibulum. Duis vehicula dolor maximus vehicula tempor. Aliquam mollis tincidunt orci cursus molestie. Duis quis feugiat massa, sed blandit metus. In sit amet dui eu elit auctor luctus a non quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras augue massa, sagittis nec justo at, placerat iaculis elit. Sed quis ullamcorper sapien. Etiam at lacus tincidunt, ornare nisl non, sodales mi. Aenean sollicitudin sem nisi, sed tristique diam sodales eu. Curabitur nec purus pellentesque, mattis metus vitae, aliquam mi. Duis molestie non mauris ac consectetur. Maecenas auctor nunc sit amet magna euismod faucibus. Donec tincidunt risus quis diam ultricies iaculis. Vivamus auctor condimentum dolor, id aliquet sapien tincidunt ut. Duis posuere libero dignissim malesuada laoreet. </p>
+			</article>
+			<article class="entrada">
+				<h2>Titulo de mi entrada</h2>
+				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu mauris ac orci auctor vestibulum. Duis vehicula dolor maximus vehicula tempor. Aliquam mollis tincidunt orci cursus molestie. Duis quis feugiat massa, sed blandit metus. In sit amet dui eu elit auctor luctus a non quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras augue massa, sagittis nec justo at, placerat iaculis elit. Sed quis ullamcorper sapien. Etiam at lacus tincidunt, ornare nisl non, sodales mi. Aenean sollicitudin sem nisi, sed tristique diam sodales eu. Curabitur nec purus pellentesque, mattis metus vitae, aliquam mi. Duis molestie non mauris ac consectetur. Maecenas auctor nunc sit amet magna euismod faucibus. Donec tincidunt risus quis diam ultricies iaculis. Vivamus auctor condimentum dolor, id aliquet sapien tincidunt ut. Duis posuere libero dignissim malesuada laoreet. </p>
+			</article>
+			<article class="entrada">
+				<h2>Titulo de mi entrada</h2>
+				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu mauris ac orci auctor vestibulum. Duis vehicula dolor maximus vehicula tempor. Aliquam mollis tincidunt orci cursus molestie. Duis quis feugiat massa, sed blandit metus. In sit amet dui eu elit auctor luctus a non quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras augue massa, sagittis nec justo at, placerat iaculis elit. Sed quis ullamcorper sapien. Etiam at lacus tincidunt, ornare nisl non, sodales mi. Aenean sollicitudin sem nisi, sed tristique diam sodales eu. Curabitur nec purus pellentesque, mattis metus vitae, aliquam mi. Duis molestie non mauris ac consectetur. Maecenas auctor nunc sit amet magna euismod faucibus. Donec tincidunt risus quis diam ultricies iaculis. Vivamus auctor condimentum dolor, id aliquet sapien tincidunt ut. Duis posuere libero dignissim malesuada laoreet. </p>
+			</article>
+			<article class="entrada">
+				<h2>Titulo de mi entrada</h2>
+				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu mauris ac orci auctor vestibulum. Duis vehicula dolor maximus vehicula tempor. Aliquam mollis tincidunt orci cursus molestie. Duis quis feugiat massa, sed blandit metus. In sit amet dui eu elit auctor luctus a non quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras augue massa, sagittis nec justo at, placerat iaculis elit. Sed quis ullamcorper sapien. Etiam at lacus tincidunt, ornare nisl non, sodales mi. Aenean sollicitudin sem nisi, sed tristique diam sodales eu. Curabitur nec purus pellentesque, mattis metus vitae, aliquam mi. Duis molestie non mauris ac consectetur. Maecenas auctor nunc sit amet magna euismod faucibus. Donec tincidunt risus quis diam ultricies iaculis. Vivamus auctor condimentum dolor, id aliquet sapien tincidunt ut. Duis posuere libero dignissim malesuada laoreet. </p>
+			</article>
+			
+			
+		</div>
+
+		</div>
+
+		<!-- FOOTER -->
+
+
+
+	</body>
+	</html>
